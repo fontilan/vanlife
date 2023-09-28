944fcec47b44966846d8ababd24cc1c48570d410
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2023-09-28 17:52:59 +0200

    feat: add routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 function App() {
   return <h1>Hello World!</h1>;
 }
 
+function About() {
+  return <h1>About page goes here!</h1>;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 );
