ffb779d36356359f2e7e3a346af56d739b3b6d0f
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2023-09-29 15:04:41 +0200

    feat: style the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,9 +2,18 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+    <nav className="flex justify-between px-4 py-8">
+      <div className="text-lg font-extrabold">
+        <Link to="/">#VANLIFE</Link>
+      </div>
+      <div className="flex gap-4 text-stone-600 font-medium">
+        <Link className="hover:text-stone-800 hover:underline" to="/about">
+          About
+        </Link>
+        <Link className="hover:text-stone-800 hover:underline" to="/vans">
+          Vans
+        </Link>
+      </div>
     </nav>
   );
 }
