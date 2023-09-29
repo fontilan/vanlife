6fd17399b25eb30a759dfc85a2b31db93911d58c
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2023-09-29 15:15:44 +0200

    feat: add footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="w-full bg-neutral-800 text-neutral-100 text-center py-6">
+      <p>C 2023 #VANLIFE</p>
+    </div>
+  );
+}
+
+export default Footer;
