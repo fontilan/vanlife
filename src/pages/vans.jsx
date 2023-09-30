60d1130332eade7cc59fa3f9bea45fd416513dbf
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2023-09-30 17:27:59 +0200

    refactor: add cleanup

--- src/pages/vans.jsx
+++ src/pages/vans.jsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from 'react'
 
 function Vans() {
-  const [vansData, setVansData] = useState([])
+  const [vansData, setVansData] = useState(null)
 
   useEffect(() => {
+    let ignore = false
+    setVansData(null)
     fetch('/api/vans')
       .then((res) => res.json())
-      .then((data) => setVansData(data))
+      .then((data) => {
+        if (!ignore) {
+          setVansData(data)
+        }
+      })
+    return () => {
+      ignore = true
+    }
   }, [])
 
   console.log(vansData)
