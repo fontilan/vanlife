c8a930ca7292a13757ce4102e3dbda5f21556feb
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2023-09-30 17:23:44 +0200

    feat: fetch the data and save it to state

--- src/pages/vans.jsx
+++ src/pages/vans.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from 'react'
+
 function Vans() {
+  const [vansData, setVansData] = useState([])
+
+  useEffect(() => {
+    fetch('/api/vans')
+      .then((res) => res.json())
+      .then((data) => setVansData(data))
+  }, [])
+
+  console.log(vansData)
   return <p>Vans go here</p>
 }
 
