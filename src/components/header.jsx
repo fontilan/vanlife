f0f74550eb4c50fd0633c5925ae5814d9c1f401d
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-10 15:11:10 +0100

    refactor: the navigation elements

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,19 +2,19 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <nav className="flex justify-between px-4 py-8">
+    <header className="flex justify-between px-4 py-8">
       <div className=" text-lg font-extrabold">
         <Link to="/">#VANLIFE</Link>
       </div>
-      <div className="flex gap-4 font-medium text-stone-600">
+      <nav className="flex gap-4 font-medium text-stone-600">
         <Link className="hover:text-stone-800 hover:underline" to="/about">
           About
         </Link>
         <Link className="hover:text-stone-800 hover:underline" to="/vans">
           Vans
         </Link>
-      </div>
-    </nav>
+      </nav>
+    </header>
   )
 }
 
