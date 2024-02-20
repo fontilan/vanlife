abc0235c17b6420ff26435ca3fe8c127897ed4d3
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-20 18:56:40 +0100

    rename the files

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
-import Header from './header'
-import Footer from './footer'
+import Header from './Header'
+import Footer from './Footer'
 
 // "An <Outlet> should be used in parent route elements to render their child route elements. This allows nested UI to show up when child routes are rendered. https://reactrouter.com/en/main/components/outlet
 
