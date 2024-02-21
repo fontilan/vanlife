0e5c56244c718dc48ad2980ba2a8924644e4b6f8
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-21 16:22:56 +0100

    fix: copyright symbol in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="w-full bg-neutral-800 py-6 text-center text-neutral-100">
-      <p>C 2023 #VANLIFE</p>
+      <p>&#169; 2023 #VANLIFE</p>
     </div>
   )
 }
