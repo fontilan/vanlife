686dd145b8172e9a83a86d30f72d37a5bd9b1578
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-21 12:37:02 +0100

    feat: import the HostLayout, allow proper linking to the nested host pages

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,7 @@ import Dashboard from './pages/Host/Dashboard'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
 import Layout from './components/Layout'
+import HostLayout from './components/HostLayout'
 import './server'
 import './index.css'
 
@@ -22,7 +23,8 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/vans" element={<Vans />} />
             <Route path="/vans/:id" element={<VanDetail />} />
-            <Route path="/host" element={<Dashboard />}>
+            <Route path="/host" element={<HostLayout />}>
+              <Route path="/host" element={<Dashboard />} />
               <Route path="/host/income" element={<Income />} />
               <Route path="/host/reviews" element={<Reviews />} />
             </Route>
