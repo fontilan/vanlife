5d7358a49467aa83554dcbef2fe177cd8680bedb
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-22 19:33:24 +0100

    feat: create pages for the host vans

---
+++ src/pages/Host/HostVanDetail.jsx
@@ -0,0 +1,9 @@
+function HostVanDetail() {
+  return (
+    <>
+      <h1>HostVanDetail page goes here</h1>
+    </>
+  )
+}
+
+export default HostVanDetail
---
+++ src/pages/Host/HostVans.jsx
@@ -0,0 +1,9 @@
+function HostVans() {
+  return (
+    <>
+      <h1>HostVans page goes here</h1>
+    </>
+  )
+}
+
+export default HostVans
