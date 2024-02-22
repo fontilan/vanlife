a78934deef795315f5f11b2d35357c916e9ad43c
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-22 19:34:13 +0100

    feat: create routes for the host vans and import those components

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,8 @@ import VanDetail from './pages/Vans/VanDetail'
 import Dashboard from './pages/Host/Dashboard'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
+import HostVans from './pages/Host/HostVans'
+import HostVanDetail from './pages/Host/HostVanDetail'
 import Layout from './components/Layout'
 import HostLayout from './components/HostLayout'
 import './server'
@@ -25,6 +27,8 @@ function App() {
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
