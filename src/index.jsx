ef551abf98a7253f964f86cae374dc7d32f5cd68
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-23 18:50:43 +0100

    feat: import the new components

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,11 @@ import Dashboard from './pages/Host/Dashboard'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
 import HostVans from './pages/Host/HostVans'
-import HostVanLayout from './pages/Host/HostVanLayout'
+import HostVanSelected from './pages/Host/HostVanSelected'
+import HostVanPhotos from './pages/Host/HostVanPhotos'
+import HostVanPricing from './pages/Host/HostVanPricing'
+import HostVanDetails from './pages/Host/HostVanDetails'
+
 import Layout from './components/Layout'
 import HostLayout from './components/HostLayout'
 import './server'
@@ -28,8 +32,10 @@ function App() {
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanLayout />}>
-              <Route index />
+            <Route path="vans/:id" element={<HostVanSelected />}>
+              <Route index element={<HostVanDetails />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
             </Route>
             <Route path="reviews" element={<Reviews />} />
           </Route>
