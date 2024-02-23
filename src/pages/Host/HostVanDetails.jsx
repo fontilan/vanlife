daacc6736bd5c213590656d4bdb16e937b34bee7
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-23 18:49:30 +0100

    rename file, add NavLinks, add new separate file for the details

--- src/pages/Host/HostVanDetails.jsx
+++ src/pages/Host/HostVanDetails.jsx
@@ -1,61 +1,8 @@
-import { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
-
 function HostVanDetails() {
-  const params = useParams()
-  const [van, setVan] = useState(null)
-
-  useEffect(() => {
-    let ignore = false
-    setVan(null)
-    fetch(`/api/host/vans/${params.id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        if (!ignore) {
-          setVan(data.vans[0])
-        }
-      })
-    return () => {
-      ignore = true
-    }
-  }, [params.id])
-
-  return van ? (
-    <div className="max-w-2xl py-4">
-      <Link
-        className="block py-4 text-stone-600 hover:text-stone-800"
-        to="/host/vans"
-      >
-        ← <span className=" hover:underline">Back to all vans</span>
-      </Link>
-      <div className="flex rounded-md bg-white p-4">
-        <img className="h-48 rounded-md" src={van.imageUrl} />
-        <div className="self-center p-4">
-          <button
-            className={
-              'block rounded-md px-6 py-1 font-medium text-orange-50 ' +
-              (van.type === 'simple'
-                ? 'bg-orange-800'
-                : '' + van.type === 'rugged'
-                  ? 'bg-emerald-800'
-                  : '' + van.type === 'luxury'
-                    ? 'bg-neutral-900'
-                    : '')
-            }
-          >
-            {van.type}
-          </button>
-          <p className="my-4 text-2xl font-extrabold">{van.name}</p>
-          <p className="">
-            <span className="text-xl font-bold">${van.price}</span>/day
-          </p>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <p className="flex h-screen w-screen items-center justify-center text-2xl font-semibold">
-      Loading...
-    </p>
+  return (
+    <>
+      <h1>HostVanDetails go here</h1>
+    </>
   )
 }
 
---
+++ src/pages/Host/HostVanSelected.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+import { useParams, Link, NavLink, Outlet } from 'react-router-dom'
+
+function HostVanSelected() {
+  const params = useParams()
+  const [van, setVan] = useState(null)
+
+  useEffect(() => {
+    let ignore = false
+    setVan(null)
+    fetch(`/api/host/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (!ignore) {
+          setVan(data.vans[0])
+        }
+      })
+    return () => {
+      ignore = true
+    }
+  }, [params.id])
+
+  return van ? (
+    <div className="max-w-2xl py-4">
+      <Link
+        className="block py-4 text-stone-600 hover:text-stone-800"
+        to="/host/vans"
+      >
+        ← <span className=" hover:underline">Back to all vans</span>
+      </Link>
+      <div className="flex rounded-md bg-white p-4">
+        <img className="h-48 rounded-md" src={van.imageUrl} />
+        <div className="self-center p-4">
+          <button
+            className={
+              'block rounded-md px-6 py-1 font-medium text-orange-50 ' +
+              (van.type === 'simple'
+                ? 'bg-orange-800'
+                : '' + van.type === 'rugged'
+                  ? 'bg-emerald-800'
+                  : '' + van.type === 'luxury'
+                    ? 'bg-neutral-900'
+                    : '')
+            }
+          >
+            {van.type}
+          </button>
+          <p className="my-4 text-2xl font-extrabold">{van.name}</p>
+          <p className="">
+            <span className="text-xl font-bold">${van.price}</span>/day
+          </p>
+        </div>
+      </div>
+      <NavLink to="">Details</NavLink>
+      <NavLink to="photos">Photos</NavLink>
+      <NavLink to="pricing">Pricing</NavLink>
+      <Outlet />
+    </div>
+  ) : (
+    <p className="flex h-screen w-screen items-center justify-center text-2xl font-semibold">
+      Loading...
+    </p>
+  )
+}
+
+export default HostVanSelected
