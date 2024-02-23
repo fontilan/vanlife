4bf9a53211196b56ccc3d1f37280ef8acde538f8
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-23 22:07:49 +0100

    style: reorder the css styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ import Footer from './Footer'
 
 function Layout() {
   return (
-    <main className="font-inter flex min-h-screen flex-col bg-orange-50">
+    <main className="flex min-h-screen flex-col bg-orange-50 font-inter">
       <Header />
       <div className="flex flex-grow">
         <Outlet />
