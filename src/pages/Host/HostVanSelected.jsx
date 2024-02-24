ac21ded1f2af0fa2d3e422fe21f37385bde3db41
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-24 12:45:56 +0100

    feat: use relative path in link, UI: adjust  the layout

--- src/pages/Host/HostVanSelected.jsx
+++ src/pages/Host/HostVanSelected.jsx
@@ -24,37 +24,40 @@ function HostVanSelected() {
     <div className="max-w-2xl py-4">
       <Link
         className="block py-4 text-stone-600 hover:text-stone-800"
-        to="/host/vans"
+        to=".."
+        relative="path"
       >
         ← <span className=" hover:underline">Back to all vans</span>
       </Link>
-      <div className="flex rounded-md bg-white p-4">
-        <img className="h-48 rounded-md" src={van.imageUrl} />
-        <div className="self-center p-4">
-          <button
-            className={
-              'block rounded-md px-6 py-1 font-medium text-orange-50 ' +
-              (van.type === 'simple'
-                ? 'bg-orange-800'
-                : '' + van.type === 'rugged'
-                  ? 'bg-emerald-800'
-                  : '' + van.type === 'luxury'
-                    ? 'bg-neutral-900'
-                    : '')
-            }
-          >
-            {van.type}
-          </button>
-          <p className="my-4 text-2xl font-extrabold">{van.name}</p>
-          <p className="">
-            <span className="text-xl font-bold">${van.price}</span>/day
-          </p>
+      <div className="rounded-md bg-white p-4">
+        <div className="flex">
+          <img className="h-48 rounded-md" src={van.imageUrl} />
+          <div className="self-center p-4">
+            <button
+              className={
+                'block rounded-md px-6 py-1 font-medium text-orange-50 ' +
+                (van.type === 'simple'
+                  ? 'bg-orange-800'
+                  : '' + van.type === 'rugged'
+                    ? 'bg-emerald-800'
+                    : '' + van.type === 'luxury'
+                      ? 'bg-neutral-900'
+                      : '')
+              }
+            >
+              {van.type}
+            </button>
+            <p className="my-4 text-2xl font-extrabold">{van.name}</p>
+            <p className="">
+              <span className="text-xl font-bold">${van.price}</span>/day
+            </p>
+          </div>
         </div>
+        <NavLink to="">Details</NavLink>
+        <NavLink to="photos">Photos</NavLink>
+        <NavLink to="pricing">Pricing</NavLink>
+        <Outlet />
       </div>
-      <NavLink to="">Details</NavLink>
-      <NavLink to="photos">Photos</NavLink>
-      <NavLink to="pricing">Pricing</NavLink>
-      <Outlet />
     </div>
   ) : (
     <p className="flex h-screen w-screen items-center justify-center text-2xl font-semibold">
