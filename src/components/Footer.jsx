88b55263fadc45afd24cfb12e80439a291d9a85d
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-25 17:37:06 +0100

    semantics: change div to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <div className="w-full bg-neutral-700 py-5 text-center text-neutral-300">
+    <footer className="w-full bg-neutral-700 py-5 text-center text-neutral-300">
       <p>&#169; 2024 #VANLIFE</p>
-    </div>
+    </footer>
   )
 }
 
