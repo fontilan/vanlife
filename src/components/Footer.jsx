df7ed9b19cf24324a8376b2534e9cad8cf0f830e
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-25 17:00:46 +0100

    UI: adjust the padding, fix the date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <div className="w-full bg-neutral-700 py-6 text-center text-neutral-300">
-      <p>&#169; 2023 #VANLIFE</p>
+    <div className="w-full bg-neutral-700 py-5 text-center text-neutral-300">
+      <p>&#169; 2024 #VANLIFE</p>
     </div>
   )
 }
