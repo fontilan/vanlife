5775f4a44fb27ca7abcec06d02aea85dc45854d8
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-29 10:25:07 +0100

    fix (fast refresh error): split the index file into 2 files

---
+++ src/app.jsx
@@ -0,0 +1,50 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Vans from './pages/Vans/Vans'
+import VanDetail from './pages/Vans/VanDetail'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
+import HostVans from './pages/Host/HostVans'
+import HostVanDetails from './pages/Host/HostVanDetails'
+import HostVanPhotos from './pages/Host/HostVanPhotos'
+import HostVanPricing from './pages/Host/HostVanPricing'
+import HostVanInfo from './pages/Host/HostVanInfo'
+import Layout from './components/Layout'
+import HostLayout from './components/HostLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="vans" element={<Vans />} />
+      <Route path="vans/:id" element={<VanDetail />} />
+      <Route path="host" element={<HostLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="income" element={<Income />} />
+        <Route path="vans" element={<HostVans />} />
+        <Route path="vans/:id" element={<HostVanDetails />}>
+          <Route index element={<HostVanInfo />} />
+          <Route path="pricing" element={<HostVanPricing />} />
+          <Route path="photos" element={<HostVanPhotos />} />
+        </Route>
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Route>,
+  ),
+)
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App
