a2fcc63542f36975e501740cc573d9a3ba79d1bf
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-02-29 17:47:24 +0100

    refactor: use loader instead of useEffect

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,41 +1,26 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
+import { getHostVans } from '../../api'
+
+export function loader() {
+  return getHostVans()
+}
 
 function HostVans() {
-  const [hostVanData, setHostVanData] = useState(null)
+  const hostVanData = useLoaderData()
 
-  useEffect(() => {
-    let ignore = false
-    setHostVanData(null)
-    fetch('/api/host/vans')
-      .then((res) => res.json())
-      .then((data) => {
-        if (!ignore) {
-          setHostVanData(data.vans)
-        }
-      })
-    return () => {
-      ignore = true
-    }
-  }, [])
-
-  let hostVansList = []
-
-  if (hostVanData != null) {
-    hostVansList = hostVanData.map((van) => (
-      <div key={van.id} className="py-2">
-        <Link to={van.id}>
-          <div className="flex w-full items-center rounded-md bg-white px-6 py-5">
-            <img className="h-28 rounded-sm" src={van.imageUrl} />
-            <div className="ml-4">
-              <p className="text-xl font-semibold">{van.name}</p>
-              <p className="text-neutral-600">${van.price}/day</p>
-            </div>
+  const hostVansList = hostVanData.map((van) => (
+    <div key={van.id} className="py-2">
+      <Link to={van.id}>
+        <div className="flex w-full items-center rounded-md bg-white px-6 py-5">
+          <img className="h-28 rounded-sm" src={van.imageUrl} />
+          <div className="ml-4">
+            <p className="text-xl font-semibold">{van.name}</p>
+            <p className="text-neutral-600">${van.price}/day</p>
           </div>
-        </Link>
-      </div>
-    ))
-  }
+        </div>
+      </Link>
+    </div>
+  ))
 
   return (
     <div className="py-4">
