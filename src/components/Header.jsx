591d8b4d5076cd34c562629ef32872e79a3cbcf0
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-03-01 15:08:31 +0100

    refactor: extract the duplicate styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,28 @@
 import { NavLink } from 'react-router-dom'
 
 function Header() {
+  /* the NavLink component returns an object with a isActive property, a boolean which value depends on whether the route is the current active route. We then use it to conditionally apply the styling. */
+  const navLinkStyle = ({ isActive }) =>
+    isActive
+      ? 'font-bold text-neutral-950 underline'
+      : 'hover:text-neutral-800 hover:underline'
+
   return (
     <header className="flex items-center justify-between px-5 py-8">
       <div className="text-3xl font-black text-neutral-950">
         <NavLink to="/">#VANLIFE</NavLink>
       </div>
       <nav className="flex gap-7 font-medium text-neutral-600">
-        {/* the NavLink component returns an object with a isActive property, a boolean which value depends on whether the route (to="/host" in the case below) is the current active route. We then use it to conditionally apply the styling. */}
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? 'font-bold text-neutral-950 underline'
-              : 'hover:text-neutral-800 hover:underline'
-          }
-          to="/host"
-        >
+        <NavLink className={navLinkStyle} to="/host">
           Host
         </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? 'font-bold text-neutral-950 underline'
-              : 'hover:text-neutral-800 hover:underline'
-          }
-          to="/about"
-        >
+        <NavLink className={navLinkStyle} to="/about">
           About
         </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? 'font-bold text-neutral-950 underline'
-              : 'hover:text-neutral-800 hover:underline'
-          }
-          to="/vans"
-        >
+        <NavLink className={navLinkStyle} to="/vans">
           Vans
         </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? 'font-bold text-neutral-950 underline'
-              : 'hover:text-neutral-800 hover:underline'
-          }
-          to="/login"
-        >
+        <NavLink className={navLinkStyle} to="/login">
           Login
         </NavLink>
       </nav>
