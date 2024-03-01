598c184ac94b571c4407a8f0b2e1c484af197da0
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-03-01 13:59:25 +0100

    remove console log

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,6 @@ import { useRouteError } from 'react-router-dom'
 
 function Error() {
   const error = useRouteError()
-  console.log(error)
   return (
     <div className="py-4">
       <h1 aria-live="assertive" className="py-4 text-3xl font-bold">
