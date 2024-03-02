ea6784bd320a97873f12e92a12e66cebe883bac9
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-03-02 22:29:29 +0100

    refactor: extract the navlink style

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,48 +1,24 @@
 import { NavLink, Outlet } from 'react-router-dom'
 
 function HostLayout() {
+  const navLinkStyle = ({ isActive }) =>
+    isActive
+      ? 'font-bold text-neutral-950 underline'
+      : 'hover:text-neutral-800 hover:underline'
+
   return (
     <div className="w-screen px-5 pb-12" id="host-content">
       <nav className="flex gap-7 py-5 font-medium text-neutral-600">
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? 'font-bold text-neutral-950 underline'
-              : 'hover:text-neutral-800 hover:underline'
-          }
-          to="/host"
-          end
-        >
+        <NavLink className={navLinkStyle} to="/host" end>
           Dashboard
         </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? 'font-bold text-neutral-950 underline'
-              : 'hover:text-neutral-800 hover:underline'
-          }
-          to="income"
-        >
+        <NavLink className={navLinkStyle} to="income">
           Income
         </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? 'font-bold text-neutral-950 underline'
-              : 'hover:text-neutral-800 hover:underline'
-          }
-          to="vans"
-        >
+        <NavLink className={navLinkStyle} to="vans">
           Vans
         </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? 'font-bold text-neutral-950 underline'
-              : 'hover:text-neutral-800 hover:underline'
-          }
-          to="reviews"
-        >
+        <NavLink className={navLinkStyle} to="reviews">
           Reviews
         </NavLink>
       </nav>
