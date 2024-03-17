07d995bc6eee54e89042bb7b0b919a2ec6ddf5cd
Wojciech Gmuzdek <wgmuzdek@gmail.com>
2024-03-17 11:51:47 +0100

    feat: add message to the redirect

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 import { redirect } from './redirect'
 
 export async function requireAuth() {
-  const isLoggedIn = true
+  const isLoggedIn = false
   if (!isLoggedIn) {
-    throw redirect('/login')
+    throw redirect('/login?message=You must log in first.')
   }
   return null
 }
